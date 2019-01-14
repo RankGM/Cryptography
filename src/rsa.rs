5a1335fc439bf0e5d0a348295ad0bdca8ddb661c
Fernanda Guimarães <fernandaguimaraes28@gmail.com>
2019-01-14 20:54:56 -0200

    Replaced while loop with "find".

--- src/rsa.rs
+++ src/rsa.rs
@@ -16,17 +16,9 @@ fn main() {
 
   // Finding other part of public key.
   // e stands for encrypt
-  let mut e: u32 = 2;
   let phi: i128 = (p-1)*(q-1);
 
-  while (e as i128) < phi {
-    if gcd(e as i128, phi) == 1 {
-      break;
-    }
-    else {
-      e += 1;
-    }
-  }
+  let e = (2..).find(|&x| gcd(x as i128, phi) == 1).unwrap();
 
   // Private key (d stands for decrypt)
   // choosing d such that it satisfies
@@ -38,13 +30,11 @@ fn main() {
   let msg: i128 = 20;
   println!("Message data = {}", msg);
   // Encryption c = (msg ^ e) % n
-  let mut c: i128 = i128::pow(msg, e);
-  // mod workaround
-  c = (c % n) +n;
+  let c: i128 = (i128::pow(msg, e) % n) + n;
   println!("\nEncrypted data = {}", c);
 
   // Decryption m = (c ^ d) % n
-  let mut m: i128 = i128::pow(c, d as u32);
-  m = ((m % n) +n) %n;
+  let m: i128 = (((i128::pow(c, d as u32)) % n) +n) %n;
+
   println!("Original Message Sent = {} ", m);
 }
